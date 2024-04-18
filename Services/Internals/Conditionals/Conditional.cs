aecc0ccf7ad1ba6f085151269836f0a9e0bf1fde
WarmasterPrime <warmasterprime.gamming@gmail.com>
2024-04-18 07:57:09 -1000

    - Added the Conditional static class to perform operations based on the result of a conditional statement, without creating multiple lines of code to establish that.

---
+++ Services/Internals/Conditionals/Conditional.cs
@@ -0,0 +1,22 @@
+﻿using VAdvanceStringLibrary.Services.Internals.Objects;
+
+namespace VAdvanceStringLibrary.Services.Internals.Conditionals
+{
+	internal static class Conditional
+	{
+		/// <summary>
+		/// Performs an operation without returning anything.
+		/// </summary>
+		/// <param name="conditionResult">The result of a condition.</param>
+		/// <param name="onTrue">The action to perform when the condition results in <see cref="bool">true</see>.</param>
+		/// <param name="onFalse">The action to perform when the condition results in <see cref="bool">false</see>.</param>
+		public static void Void(bool conditionResult, Action onTrue, Action? onFalse = null)
+		{
+			if(conditionResult)
+				onTrue.Invoke();
+			else if(onFalse.NotNull())
+				onFalse!.Invoke();
+		}
+
+	}
+}
