45cef9c48d3cdfe5eb162f0db5d0c0ef51f338d6
Daniel K. Valente <dan.k.valente@gmail.com>
2024-08-31 21:23:22 +0200

    - Updated the documentation.

--- StringContainsAnyExt.cs
+++ StringContainsAnyExt.cs
@@ -8,16 +8,16 @@
 		/// <summary>
 		/// Determines if any of the <paramref name="values"/> were found in the <paramref name="value"/>.
 		/// </summary>
-		/// <param name="value"></param>
-		/// <param name="values"></param>
-		/// <returns></returns>
+		/// <param name="value">The <see cref="string"/> value to look through.</param>
+		/// <param name="values">The <see cref="string"/> values to look for.</param>
+		/// <returns>a <see cref="bool"/> representing the result from the search.</returns>
 		public static bool ContainsAny(this string value, params string[] values) => !value.IsEmpty() && values.IsValid() && values.Any(q=>value.Contains(q));
 		/// <summary>
 		/// Determines if all of the <paramref name="values"/> were found in the <paramref name="value"/>.
 		/// </summary>
-		/// <param name="value"></param>
-		/// <param name="values"></param>
-		/// <returns></returns>
+		/// <param name="value">The <see cref="string"/> value to look through.</param>
+		/// <param name="values">The <see cref="string"/> values to look for.</param>
+		/// <returns>a <see cref="bool"/> representing the result from the search.</returns>
 		public static bool ContainsAll(this string value, params string[] values) => !value.IsEmpty() && values.IsValid() && values.All(q=>value.Contains(q));
 
 	}
