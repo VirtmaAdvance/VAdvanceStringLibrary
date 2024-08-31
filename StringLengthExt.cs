b5e22ad8633f5e932bfed4d2824e8062d6552901
Daniel K. Valente <dan.k.valente@gmail.com>
2024-08-31 21:32:56 +0200

    - Added null parameter to the argument for string to accept null string values.

--- StringLengthExt.cs
+++ StringLengthExt.cs
@@ -10,7 +10,7 @@
 		/// </summary>
 		/// <param name="value">A <see cref="string"/> value.</param>
 		/// <returns>the length of the <see cref="string"/> value.</returns>
-		public static int Count(this string value) => value?.Length??0;
+		public static int Count(this string? value) => value?.Length??0;
 
 	}
 }
