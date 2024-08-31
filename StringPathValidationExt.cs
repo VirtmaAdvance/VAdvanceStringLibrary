5f78e4d66e0555367580c24fc79fb1d1a19858f0
Daniel K. Valente <dan.k.valente@gmail.com>
2024-08-31 21:34:03 +0200

    - Updated documentation.

--- StringPathValidationExt.cs
+++ StringPathValidationExt.cs
@@ -8,8 +8,8 @@
 		/// <summary>
 		/// Determines if the <paramref name="value"/> references an existing file or directory path.
 		/// </summary>
-		/// <param name="value"></param>
-		/// <returns></returns>
+		/// <param name="value">The <see cref="string"/> representation of a file system path.</param>
+		/// <returns>a <see cref="bool"/> value representation of the result from the validation where <see cref="bool">true</see> represents success, and <see cref="bool">false</see> represents failure.</returns>
 		public static bool PathExists(this string value) => value.IsFile() || value.IsDirectory();
 
 	}
