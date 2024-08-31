497992dfd86fa34376c91df9c06fe2f43cba2b24
Daniel K. Valente <dan.k.valente@gmail.com>
2024-08-31 21:28:44 +0200

    - Updated documentation.

--- StringDirectoryValidationExt.cs
+++ StringDirectoryValidationExt.cs
@@ -8,8 +8,8 @@
 		/// <summary>
 		/// Determines if the <paramref name="value"/> references an existing directory path.
 		/// </summary>
-		/// <param name="value"></param>
-		/// <returns></returns>
+		/// <param name="value">The <see cref="string"/> representation of the directory path to validate.</param>
+		/// <returns>a <see cref="bool"/> value representation of the result from the validation where <see cref="bool">true</see> represents success, and <see cref="bool">false</see> represents failure.</returns>
 		public static bool IsDirectory(this string value) => value.IsValid() && Directory.Exists(value);
 
 	}
